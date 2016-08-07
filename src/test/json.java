15579a540254150100cb909e372172d41eefbfa1
449005549@qq.com <449005549@qq.com>
2016-08-07 15:42:32 +0800

    asd

---
+++ src/test/json.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+public class json {
+	
+	
+	public static void test() throws IOException{
+		JsonFactory factory = new JsonFactory();
+		ObjectMapper mapper = new ObjectMapper(factory);
+	    TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {};
+		String dd;
+		
+	    BufferedReader br = new BufferedReader(new FileReader("¸´¼þ (1) 2015-01-01-15.json") ); 
+	    if(br!=null){
+	    	
+				while((dd=br.readLine())!=null ){
+					HashMap<String, Object> map = mapper.readValue(dd, typeRef);
+					String s=mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+					System.out.println(s);
+					
+				//for (Entry<String, Object> entry : map.entrySet()) {  
+					  
+					   ///System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());  
+					  
+					  
+				  }
+			} 			}}
+
+	
+	
+
---
+++ src/test/test1.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.io.IOException;
+
+public class test1 {
+
+	public static void main(String[] args) throws IOException {
+		json a=new json();
+		a.test();
+
+	}
+
+}
