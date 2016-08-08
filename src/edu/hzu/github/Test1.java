56b4e7dc1a35d79059be43da18acc2955d3a1ab2
zengsn <shaoning.zeng@gmail.com>
2016-08-08 20:11:39 +0800

    调整类名，包名

---
+++ src/edu/hzu/github/Test1.java
@@ -0,0 +1,15 @@
+package edu.hzu.github;
+
+import java.io.IOException;
+
+import edu.hzu.github.JsonData;
+
+public class Test1 {
+
+	public static void main(String[] args) throws IOException {
+		JsonData json=new JsonData();
+		json.test();
+
+	}
+
+}
