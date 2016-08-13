35285d740e04ef3560197ee38fbdfff7da31e6af
zengsn <shaoning.zeng@gmail.com>
2016-08-13 19:20:53 +0800

    JSON处理便利类

---
+++ src/edu/hzu/github/JsonUtils.java
@@ -0,0 +1,100 @@
+
+package edu.hzu.github;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
+import org.codehaus.jackson.type.TypeReference;
+
+/**
+ * @author zengsn
+ * @since 7.0
+ */
+public abstract class JsonUtils {
+
+	public static String toString(Object result) {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		try {
+			return ow.writeValueAsString(result);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	public static String toString(Object result, boolean prettyPrinter) {
+		if (prettyPrinter) {
+			return toString(result);
+		}
+		ObjectWriter ow = new ObjectMapper().writer();
+		try {
+			return ow.writeValueAsString(result);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	public static Map<String, Object> toMap(String json) {
+		JsonFactory factory = new JsonFactory();
+		ObjectMapper mapper = new ObjectMapper(factory);
+		TypeReference<HashMap<String, Object>> typeRef = //
+		new TypeReference<HashMap<String, Object>>() {
+		};
+		try {
+			HashMap<String, Object> map = mapper.readValue( //
+					new ByteArrayInputStream( //
+							json.getBytes("UTF-8")), //
+					typeRef);
+			return map;
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Map<String, Object> toList(String json) {
+		JsonFactory factory = new JsonFactory();
+		ObjectMapper mapper = new ObjectMapper(factory);
+		TypeReference<ArrayList<HashMap<String, Object>>> typeRef = //
+		new TypeReference<ArrayList<HashMap<String, Object>>>() {
+		};
+		try {
+			HashMap<String, Object> map = mapper.readValue( //
+					new ByteArrayInputStream( //
+							json.getBytes("UTF-8")), //
+					typeRef);
+			return map;
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
